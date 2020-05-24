a4e4b9f090ac4f5a28f29cf8a5fca3dab232f8f3
milafilip <milafilipovwork1@gmail.com>
2020-05-24 14:03:11 +0100

    initial commit

---
+++ 01-hello-world/README.md
@@ -0,0 +1,2 @@
+emcc hello.c --emrun -o hello.html
+emrun --browser firefox hello.html
---
+++ 01-hello-world/hello.c
@@ -0,0 +1,17 @@
+// book code
+
+#include <emscripten.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+  printf("hello wasm\n");
+}
+
+// webassembly.org code
+
+// #include <stdio.h>
+
+// int main(int argc, char ** argv) {
+//   printf("Hello World\n");
+// }
